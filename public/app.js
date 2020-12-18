2921856b3e0c5176d761c058c0b5c3218605e8d0
Will Jones <willjones2010@gmail.com>
2020-12-17 21:11:40 -0500

    firestore dbgit add .

--- public/app.js
+++ public/app.js
@@ -1,20 +1,17 @@
 console.log(firebase)
 
-
+// user sign in and signout via firebase authentication
 const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
-
 const signInButton = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
-
 const userDetails = document.getElementById('userDetails');
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
 signInButton.onclick = () => auth.signInWithPopup(provider);
-
 signOutBtn.onclick = () => auth.signOut()
 
 
@@ -32,3 +29,38 @@ auth.onAuthStateChanged(user => {
         userDetails.innerHTML = '';
     }
 });
+
+
+
+// manage notes via firestore
+const db = firebase.firestore();
+
+const createNote = document.getElementById('createNote')
+const noteList = document.getElementById('notesList')
+const noteDiv = document.getElementById('noteDiv')
+const noteInput = document.getElementById('noteInput')
+let notesRef;
+let unsubscribe;
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+
+        const { serverTimestamp } = firebase.firestore.FieldValue;
+
+        noteDiv.hidden = false
+        notesRef = db.collection('notes')
+        createNote.onclick = () => {
+            console.log("my note", noteInput.value)
+            notesRef.add({
+                uid: user.uid,
+                descripton: noteInput.value,
+                // use servertimeStamp instead of Date.now() so that date obj is consistent across all client devices
+                createdAt: serverTimestamp()
+            });
+        }
+    }
+});
+
+
+
