f655c3455630989ed08401f2b322f07f291d3c74
Will Jones <willjones2010@gmail.com>
2020-12-17 20:39:39 -0500

    sign in and out

--- public/app.js
+++ public/app.js
@@ -18,8 +18,17 @@ signInButton.onclick = () => auth.signInWithPopup(provider);
 signOutBtn.onclick = () => auth.signOut()
 
 
-// auth.onAuthStateChanged(user => {
-//     if (user) {
-//         // signed in user's info
-//     }
-// })
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log("user", user)
+        // signed in user's info
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        signOutBtn.hidden = false
+        userDetails.innerHTML = `<h3> Hi ${user.displayName}! <p>User Id:${user.uid}</p>`;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
